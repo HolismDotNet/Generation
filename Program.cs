f34a6b675cd7e87d3fbee411c650f15f4a53d3c0
Saeed Nemati <sn.1361@gmail.com>
2022-02-03 15:40:23 +0330

    committed by script

--- Program.cs
+++ Program.cs
@@ -9,33 +9,12 @@ global using System.Net.Http.Headers;
 global using System.Linq;
 global using System.Linq.Expressions;
 
-namespace Generation;
+Generator.RepositoryPath = args[0];
+Generator.Organization = args[1];
+Generator.OrganizationPrefix = args[2];
+Generator.Repository = args[3];
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // args = new string[] {
-        //     "/SadraTrade/Opportunities",
-        //     "SadraTrade",
-        //     "Sadra",
-        //     "Opportunities"
-        // };
-
-        if (args.Length < 4)
-        {
-            Logger.LogWarning($"Wrong arguments are specified for the generator");
-            return;
-        }
-                    
-        Generator.RepositoryPath = args[0];
-        Generator.Organization = args[1];
-        Generator.OrganizationPrefix = args[2];
-        Generator.Repository = args[3];
-
-        new DatabaseGenerator().GenerateDatabase();
-        new ModelGenerator().GenerateModels();
-        new ContextGenerator().GenerateContext();
-        new RepositoryGenerator().GenerateRepository();
-    }
-}
+new DatabaseGenerator().GenerateDatabase();
+new ModelGenerator().GenerateModels();
+new ContextGenerator().GenerateContext();
+new RepositoryGenerator().GenerateRepository();
